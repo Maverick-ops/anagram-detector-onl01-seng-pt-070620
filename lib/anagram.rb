7c51395cb89d4a0d623eb0d6a8f51a89b84a8f88
Jesse Murphy <jessemurphy1982@gmail.com>
2020-08-04 01:06:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,6 +4,11 @@ class Anagram
   
   def initialize(word)
     @word = word
+    
+  def match(array_of_words)
+    array_of_words.select do |element|
+      (word.split("").sort) == element.split ("").sort)
+    end 
   end
   
 
