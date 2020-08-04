afeffb0ac22fbc697808ba6af42a998e6cd3b0a9
Jesse Murphy <jessemurphy1982@gmail.com>
2020-08-04 01:30:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
     
   def match(array_of_words)
     array_of_words.select do |word|
-      word.split("").sort == @word.split ("").sort
+      word.split("").sort == @word.split("").sort
     end 
   end
   
